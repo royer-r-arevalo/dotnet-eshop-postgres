c5946bd4f17dcdfae5126c92933f5399701f502a
royer-r-arevalo <royer.r.arevalo@gmail.com>
2024-04-23 09:16:02 -0400

    The Best Way To Write Parameterized Tests in .NET Using xUnit

---
+++ tests/Domain.UnitTests/Orders/OrderTests.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Customers;
+using Domain.Orders;
+
+namespace Domain.UnitTests.Orders;
+
+public class OrderTests
+{
+    [Theory]
+    [ClassData(typeof(OrderCreateTestData))]
+    public void Create_Should_RaiseDomainEvent(CustomerId customerId)
+    {
+        //Arrange
+        
+        //Act
+        var order = Order.Create(customerId);
+        
+        //Assert
+        Assert.NotEmpty(order.DomainEvents.OfType<OrderCreatedDomainEvent>());
+    }
+}
+
+
+public class OrderCreateTestData : TheoryData<CustomerId>
+{
+    public OrderCreateTestData()
+    {
+        Add(new CustomerId(Guid.NewGuid()));
+    }
+}
---
+++ tests/Domain.UnitTests/Products/SkuTests.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Products;
+
+namespace Domain.UnitTests.Products;
+
+public class SkuTests
+{
+    // [ThingUnderTest]_Should_[ExpectResult]_[Conditions]
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Create_Should_ReturnNull_WhenValueIsNullOrEmpty(string? value)
+    {
+        //Arrange
+
+        //Act
+        var sku = Sku.Create(value);
+
+        //Assert
+        Assert.Null(sku);
+    }
+
+    public static IEnumerable<object[]> InvalidSkuLengthData = new List<object[]>
+    {
+        new object[] { "invalid_sku" },
+        new object[] { "invalid_sku_1" },
+        new object[] { "invalid_sku_2" },
+    };
+
+    [Theory]
+    [MemberData(nameof(InvalidSkuLengthData))]
+    public void Create_Should_ReturnNull_WhenValueLengthIsInvalid(string value)
+    {
+        //Arrange
+        //Act
+        var sku = Sku.Create(value);
+        Assert.Null(sku);
+    }
+}
